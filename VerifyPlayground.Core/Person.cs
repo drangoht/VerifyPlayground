eaa602f1d407134d8842ab29bc27a0f0cb07d2d6
Grégory Thognard <drangoht@gmail.com>
2024-11-12 14:57:17 +0100

    Add project files.

---
+++ VerifyPlayground.Core/Person.cs
@@ -0,0 +1,32 @@
+﻿namespace VerifyPlayground.Core
+{
+    public class Person
+    {
+        public Guid Id { get; init; }
+        public string LastName { get; init; }
+        public string FirstName { get; private set; }  
+
+
+        public int BirthYear { get; init; }
+
+        public Person(Guid id, string firstName, string lastName, int birthYear)
+        {
+            if(id == Guid.Empty)
+            {
+                id = Guid.NewGuid();
+            }
+
+            FirstName = firstName;
+            LastName = lastName;
+            BirthYear = birthYear;
+        }
+
+        public int GetAge()
+        {
+            return DateTime.UtcNow.Year-BirthYear;
+        }
+
+        public void UpdateFirstname(string firstName) => FirstName=firstName;
+
+    }
+}
---
+++ VerifyPlayground.Tests/PersonTests.cs
@@ -0,0 +1,34 @@
+using VerifyPlayground.Core;
+
+namespace VerifyPlayground.Tests
+{
+    public class PersonTests
+    {
+        private Person _sut;
+        private string _firstName ="Toto";
+        private string _lastName = "Tata";
+        private Guid _id;
+        private int birthYear = 1977;
+        private VerifySettings _verifySettings;
+        public PersonTests()
+        {
+            _id = Guid.Parse("0a238b62-67b0-4bba-8e76-db88ca21f097");
+            _sut = new(_id,_firstName,_lastName, 1977);
+            _verifySettings = new();
+            _verifySettings.UseDirectory("snapshots");
+        }
+        [Fact]
+        public async Task GetAgeShouldReturnCorrectAge()
+        {
+            var correctAge = _sut.GetAge();
+            await Verify(correctAge, _verifySettings);
+        }
+
+        [Fact]
+        public async Task UpdateFirstNameShouldUpdatePerson()
+        {
+            _sut.UpdateFirstname("Titi");
+            await Verify(_sut, _verifySettings);
+        }
+    }
+}
